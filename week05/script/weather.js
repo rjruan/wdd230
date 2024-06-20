46f060a7064add865b0a2cd1731f6ee5eb29b310
rjruan <rua22001@byui.edu>
2024-06-20 10:51:27 -0600

    weather

---
+++ week05/script/weather.js
@@ -0,0 +1,31 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url ='https://api.openweathermap.org/date/2.5/df761ea8c97475cb4b1b4481df9eead2';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      // displayResults(data); // uncomment when ready
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].______;
+  weatherIcon.setAttribute('___', _____);
+  weatherIcon.setAttribute('___', _____);
+  captionDesc.textContent = `${desc}`;
+}
