b98fd8ded6bd65e858d9ffb9458ccd564126e882
rjruan <rua22001@byui.edu>
2024-06-26 14:54:05 -0600

    weather

--- week05/script/weather.js
+++ week05/script/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url ='https://api.openweathermap.org/date/2.5/df761ea8c97475cb4b1b4481df9eead2';
+const url ='https://api.openweathermap.org/data/2.5/weather?lat=25.03356159786495&lon=121.54829991170035&appid=df761ea8c97475cb4b1b4481df9eead2&units=imperial';
 
 async function apiFetch() {
   try {
@@ -10,7 +10,7 @@ async function apiFetch() {
     if (response.ok) {
       const data = await response.json();
       console.log(data); // testing only
-      // displayResults(data); // uncomment when ready
+      displayResults(data); // uncomment when ready
     } else {
         throw Error(await response.text());
     }
@@ -24,8 +24,8 @@ apiFetch();
 function displayResults(data) {
   currentTemp.innerHTML = `${data.main.temp}&deg;F`;
   const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-  let desc = data.weather[0].______;
-  weatherIcon.setAttribute('___', _____);
-  weatherIcon.setAttribute('___', _____);
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
   captionDesc.textContent = `${desc}`;
 }
