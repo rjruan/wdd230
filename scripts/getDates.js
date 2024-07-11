d46974e0b2b60191dd5d31aec75d5db33153b13d
rjruan <rua22001@byui.edu>
2024-07-10 21:18:51 -0600

    darkmode

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -14,3 +14,6 @@ document.getElementById("hamburger-toggle").addEventListener("click", function()
 document.getElementById("dark-mode-toggle").addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
 });
+document.getElementById('dark-mode-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+});
