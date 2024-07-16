6cf1d0933f777f905f907dfe4583c6c306968859
rjruan <rua22001@byui.edu>
2024-07-16 10:58:32 -0600

    chamber

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,13 +4,24 @@ document.getElementById("lastModified").textContent = "Last Modified: " + docume
 
 const heading = document.querySelector('.heading');
 
-// JavaScript for hamburger menu toggle
-document.getElementById("hamburger-toggle").addEventListener("click", function() {
-    // document.querySelector("nav ul").classList.toggle("show");
-    heading.classList.toggle('show');
-});
+// // JavaScript for hamburger menu toggle
+// document.getElementById("hamburger-toggle").addEventListener("click", function() {
+//     // document.querySelector("nav ul").classList.toggle("show");
+//     heading.classList.toggle('show');
+// });
  
 // JavaScript for dark mode toggle
 document.getElementById("dark-mode-toggle").addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
 });
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('#myButton');
+    if (button) {
+      button.addEventListener('click', () => {
+        button.classList.add('new-class');
+      });
+    } else {
+      console.error('Element not found');
+    }
+  });
+  
